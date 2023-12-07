64945adf6b9625c53ab4cf2a01e42733719ab765
tranvietdung110102 <tranvietdung10072018@gmail.com>
2023-12-07 23:30:41 +0700

    upload code

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Women Day';
+const titleCard = 'Gửi ngàn yêu thương tới bé';
+const contentCard = 'Chúc bé yêu của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'khanhlinh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
